6f31b59d486b77cc3178c80d65f6deae404990b9
kushalbhatia_jktech <kushalbhatia169@gmail.com>
2022-01-02 20:14:33 +0530

    instruction

--- utils/email.js
+++ utils/email.js
@@ -3,7 +3,6 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (email, subject, otp) => {
   // create reusable transporter object using the default SMTP transport
   console.log("Sending email...", email, subject, otp);
-  console.log(process.env.BASE_URL);
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.HOST,
