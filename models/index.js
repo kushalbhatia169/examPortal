846c5df7a3e871ea71b3fb0d30964b0d2552a3de
kushalbhatia_jktech <kushalbhatia169@gmail.com>
2022-01-02 16:41:18 +0530

    exam section

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 const dbconfig = require('../db/db.config');
-//const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI;
 mongoose
-  .connect( /* uri || */ dbconfig.DBAddress, {...dbconfig.DBPARAMS})
+  .connect( uri ||  dbconfig.DBAddress, {...dbconfig.DBPARAMS})
   .then(() => console.log('connection successfull'))
   .catch(e => {
     console.error('Connection error', e.message)
